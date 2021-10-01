c1ce0f0f5a2193dd87801f13bac3d970dd478cd6
Ryan Gibson <gibsorya@gmail.com>
2021-10-01 00:33:27 -0500

    removed comments

--- src/engine/vk/instances.hpp
+++ src/engine/vk/instances.hpp
@@ -12,32 +12,4 @@ namespace flow::vulkan {
     VkInstance createInstance();
 }
 
-// typedef struct FlowInstances FlowInstances;
-
-// namespace flow {
-// struct FlowInstances {
-// public:
-//     // [[nodiscard]] const VkInstance& findInstance() const;
-//     // [[nodiscard]] const std::vector<VkInstance>& getInstances() const;
-//     [[nodiscard]] const VkInstance& getInstance() const;
-//     void createInstance();
-//     // VkInstance createInstances();
-//     // FlowInstances();
-//     // std::shared_ptr<VkInstance> flowInstance = std::make_shared<VkInstance>();
-    
-//     // vk::Instance vkInstance;
-
-// private:
-//     VkInstance instance;
-//     std::vector<VkInstance> instances;
-// };
-
-// }
-
-// namespace flow::vulkan{
-//     // void startFlow();
-//     void createInstance();
-    
-// }
-
 #endif
