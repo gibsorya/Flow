34a6268965ff13582087370622ba3f77377fb084
Ryan Gibson <gibsorya@gmail.com>
2021-11-19 17:12:57 -0600

    Updated shaders

--- data/shaders/compile.bat
+++ data/shaders/compile.bat
@@ -1,3 +1,5 @@
 C:/VulkanSDK/1.2.189.2/Bin/glslc.exe helloTriangle.vert -o helloTriangleVert.spv
 C:/VulkanSDK/1.2.189.2/Bin/glslc.exe helloTriangle.frag -o helloTriangleFrag.spv
+C:/VulkanSDK/1.2.189.2/Bin/glslc.exe main.vert -o mainVert.spv
+C:/VulkanSDK/1.2.189.2/Bin/glslc.exe main.frag -o mainFrag.spv
 pause   
