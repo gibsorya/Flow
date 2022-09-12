d0a3d05de17a5598726b83bff6a69e09959d8684
Ryan Gibson <gibsorya@gmail.com>
2022-09-12 00:30:49 -0500

    updated compile script for windows

--- data/shaders/compile.bat
+++ data/shaders/compile.bat
@@ -1,5 +1,7 @@
-C:/VulkanSDK/1.2.189.2/Bin/glslc.exe helloTriangle.vert -o helloTriangleVert.spv
-C:/VulkanSDK/1.2.189.2/Bin/glslc.exe helloTriangle.frag -o helloTriangleFrag.spv
-C:/VulkanSDK/1.2.189.2/Bin/glslc.exe main.vert -o mainVert.spv
-C:/VulkanSDK/1.2.189.2/Bin/glslc.exe main.frag -o mainFrag.spv
+C:/VulkanSDK/1.3.224.1/Bin/glslc.exe helloTriangle.vert -o helloTriangleVert.spv
+C:/VulkanSDK/1.3.224.1/Bin/glslc.exe helloTriangle.frag -o helloTriangleFrag.spv
+C:/VulkanSDK/1.3.224.1/Bin/glslc.exe main.vert -o mainVert.spv
+C:/VulkanSDK/1.3.224.1/Bin/glslc.exe main.frag -o mainFrag.spv
+C:/VulkanSDK/1.3.224.1/Bin/glslc.exe shader.vert -o vert.spv
+C:/VulkanSDK/1.3.224.1/Bin/glslc.exe shader.frag -o frag.spv
 pause   
