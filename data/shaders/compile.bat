70501e5595ac1de5875fd1a4c9f4d0d79d635b50
Ryan Gibson <ryan@gibs.gg>
2026-07-13 19:49:03 -0400

    basic cmake structure

--- data/shaders/compile.bat
+++ data/shaders/compile.bat
@@ -1,5 +1,5 @@
-C:/VulkanSDK/1.2.189.2/Bin/glslc.exe helloTriangle.vert -o helloTriangleVert.spv
-C:/VulkanSDK/1.2.189.2/Bin/glslc.exe helloTriangle.frag -o helloTriangleFrag.spv
-C:/VulkanSDK/1.2.189.2/Bin/glslc.exe main.vert -o mainVert.spv
-C:/VulkanSDK/1.2.189.2/Bin/glslc.exe main.frag -o mainFrag.spv
-pause   
+@REM C:/VulkanSDK/1.2.189.2/Bin/glslc.exe helloTriangle.vert -o helloTriangleVert.spv
+@REM C:/VulkanSDK/1.2.189.2/Bin/glslc.exe helloTriangle.frag -o helloTriangleFrag.spv
+@REM C:/VulkanSDK/1.2.189.2/Bin/glslc.exe main.vert -o mainVert.spv
+@REM C:/VulkanSDK/1.2.189.2/Bin/glslc.exe main.frag -o mainFrag.spv
+@REM pause   
