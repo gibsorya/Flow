137d7a606d041d50b6f1e054e155b631dca288f6
Ryan Gibson <ryan@gibs.gg>
2026-07-13 19:22:28 -0400

    remove old project

--- src/platform/macos/macos_main.cpp
+++
@@ -1,4 +0,0 @@
-//
-// Created by Ryan Gibson on 3/23/22.
-//
-
--- src/platform/macos/os_mac.hpp
+++
@@ -1,8 +0,0 @@
-//
-// Created by Ryan Gibson on 3/23/22.
-//
-
-#ifndef FLOW_OS_MAC_HPP
-#define FLOW_OS_MAC_HPP
-
-#endif //FLOW_OS_MAC_HPP
